b0f56e1e21ad4faa1403808b773334db3f2d7ad5
Yotic <yoticcitoy@gmail.com>
2023-03-05 20:10:29 +0300

    Made a sample

---
+++ Sample/EntryPoint.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample;
+public class EntryPoint
+{
+    private static string logPath = @"C:\log.txt";
+    public static void Log(object obj)
+    {
+        File.AppendAllText(logPath, $"{obj}\n");
+    }
+
+    public void Load()
+    {
+        File.WriteAllText(logPath, "");
+        Log($"Injected at {DateTime.Now}");
+
+
+    }
+
+    public void Unload()
+    {
+        Log($"Uninjected at {DateTime.Now}");
+    }
+}
---
+++ Sample/Hood/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace Sample;
+
+public class Program
+{
+    public static void Main() { }
+
+    [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+    private static extern int MessageBox(IntPtr hWnd, string text, string caption, uint type);
+
+    private static EntryPoint entryPoint = new EntryPoint();
+
+    private const uint DLL_PROCESS_DETACH = 0;
+    private const uint DLL_PROCESS_ATTACH = 1;
+    private const uint DLL_THREAD_ATTACH = 2;
+    private const uint DLL_THREAD_DETACH = 3;
+
+    private static bool loaded;
+    private static bool unloaded;
+
+    [UnmanagedCallersOnly(EntryPoint = "DllMain", CallConvs = new Type[] { typeof(CallConvStdcall) })]
+    public static bool DllMain(IntPtr module, uint reason, IntPtr reserved)
+    {
+        try
+        {
+            switch (reason)
+            {
+                case DLL_PROCESS_DETACH:
+                    if (!loaded || unloaded)
+                        return false;
+
+                    unloaded = true;
+                    entryPoint.Unload();
+                    break;
+                case DLL_PROCESS_ATTACH:
+                    if (loaded)
+                        return false;
+
+                    loaded = true;
+                    entryPoint.Load();
+                    break;
+                case DLL_THREAD_ATTACH: break;
+                case DLL_THREAD_DETACH: break;
+            }
+        } catch (Exception ex) { MessageBox(0, ex.ToString(), "C# Exception", 0); }
+
+        return true;
+    }
+}
