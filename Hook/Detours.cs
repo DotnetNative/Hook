9566d66d3a0853f0c42f7d7054e23983b83969ed
Yotic <yoticcitoy@gmail.com>
2023-03-23 20:14:20 +0300

    Removed loading Detours.dll from file hosting. Now Detours.dll is loaded from the main dll folder

--- Hook/Detours.cs
+++ Hook/Detours.cs
@@ -1,4 +1,5 @@
-﻿using System.Net;
+﻿using System.Diagnostics;
+using System.Net;
 
 namespace Hook;
 public unsafe class HookApi
@@ -13,12 +14,7 @@ public unsafe class HookApi
 
     public static void Load()
     {
-        string detoursPath = Path.Combine(Path.GetTempPath(), "Detours.dll");
-        if (!File.Exists(detoursPath))
-            new WebClient().DownloadFile("https://cdn.discordapp.com/attachments/974483048546590750/1088275166200594442/Detours.dll", detoursPath);
-
-        if ((detours = Interop.GetModuleHandle("Detours")) == IntPtr.Zero)
-            detours = Interop.LoadLibrary(detoursPath);
+        detours = LoadDetours();
 
         detourRestoreAfterWith = (delegate* unmanaged<int>)Interop.GetProcAddressPtr(detours, "DllDetourRestoreAfterWith");
         detourAttach = (delegate* unmanaged<void**, void*, IntPtr>)Interop.GetProcAddressPtr(detours, "DetourAttach");
@@ -26,6 +22,43 @@ public unsafe class HookApi
         detourTransactionBegin = (delegate* unmanaged<int>)Interop.GetProcAddressPtr(detours, "DetourTransactionBegin");
         detourUpdateThread = (delegate* unmanaged<IntPtr, int>)Interop.GetProcAddressPtr(detours, "DetourUpdateThread");
         detourTransactionCommit = (delegate* unmanaged<int>)Interop.GetProcAddressPtr(detours, "DetourTransactionCommit");
+    }
+
+    private static IntPtr LoadDetours()
+    {
+        IntPtr handle;
+        if ((handle = Interop.GetModuleHandle("Detours")) != IntPtr.Zero)
+            return handle;
+
+        try
+        {
+            List<string> checkedPaths = new List<string>();
+            foreach (ProcessModule module in Process.GetCurrentProcess().Modules)
+            {
+                string path = Path.GetDirectoryName(module.FileName);
+                if (checkedPaths.Contains(path))
+                    continue;
+
+                checkedPaths.Add(path);
+
+                if (path.Contains(@"C:\Windows\"))
+                    continue;
+
+                string[] files = Directory.GetFiles(path);
+                foreach (string file in files)
+                {
+                    if (Path.GetFileName(file) == "Detours.dll")
+                    {
+                        return Interop.LoadLibrary(file);
+                    }
+                }
+            }
+
+            Interop.MessageBox(0, "Could not find Detours.dll near the main dll", "C# Exception", 0);
+        }
+        catch (Exception ex) { Interop.MessageBox(0, ex.Message, "C# Exception", 0); }
+
+        return IntPtr.Zero;
     }
 
     private static IntPtr detours;
